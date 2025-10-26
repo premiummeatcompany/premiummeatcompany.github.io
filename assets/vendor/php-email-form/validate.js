36f267e74f9e230e2de170544de79ea9d274e75f
baalintnagy <baalint.nagy@gmail.com>
2025-10-26 06:07:47 +0100

    Contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -15,7 +15,7 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      let recaptcha = false; // thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!');
@@ -56,30 +56,25 @@
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
+      thisForm.reset();
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      return "Thank you for your message!"
     })
     .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
+      
     })
     .catch((error) => {
-      displayError(thisForm, error);
+      thisForm.reset();
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      //displayError(thisForm, error);
     });
   }
 
   function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    //thisForm.querySelector('.loading').classList.remove('d-block');
+    //thisForm.querySelector('.error-message').innerHTML = error;
+    //thisForm.querySelector('.error-message').classList.add('d-block');
+    //thisForm.querySelector('.sent-message').classList.add('d-block');
   }
 
 })();
